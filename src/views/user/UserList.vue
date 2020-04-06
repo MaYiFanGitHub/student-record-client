f07ec28a8f9d6f07cfb9afa6d0eca5889233cc9a
mayifan8 <mayifan@163.com>
2020-04-06 17:31:03 +0800

    用户管理待完善

--- src/views/user/UserList.vue
+++ src/views/user/UserList.vue
@@ -1,11 +1,119 @@
 <template>
-  <div class="user-list">用户列表</div>
+  <div class="user-list">
+    <div class="user-list-header">
+      <el-form :inline="true" :model="formInline" class="demo-form-inline">
+        <el-form-item label="审批人">
+          <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域">
+          <el-select v-model="formInline.region" placeholder="活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">查询</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div class="user-list-footer">
+      <el-table :data="tableData" stripe style="width: 100%">
+        <el-table-column prop="date" label="日期" width="180">
+        </el-table-column>
+        <el-table-column prop="name" label="姓名" width="180">
+        </el-table-column>
+        <el-table-column prop="address" label="地址"> </el-table-column>
+        <el-table-column fixed="right" label="操作" width="150">
+          <template slot-scope="scope">
+            <el-button
+              type="primary"
+              @click="handleClick(scope.row, true)"
+              icon="el-icon-edit"
+              circle
+              size="mini"
+            ></el-button>
+            <el-button
+              type="danger"
+              @click="handleClick(scope.row, false)"
+              icon="el-icon-delete"
+              circle
+              size="mini"
+            ></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        background
+        :hide-on-single-page="true"
+        layout="prev, pager, next"
+        :total="1000"
+      >
+      </el-pagination>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      formInline: {
+        user: "",
+        region: ""
+      },
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        }
+      ]
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("submit!");
+    },
+    handleClick(row, flag) {
+      if (flag) {
+        // 编辑
+      } else {
+        // 删除
+        this.$confirm("确定要删除此记录吗？是否继续?", "提示", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning"
+        })
+          .then(() => {
+            this.$message({
+              type: "success",
+              message: "删除成功!"
+            });
+          })
+          .catch(() => {
+            this.$message({
+              type: "info",
+              message: "已取消删除"
+            });
+          });
+      }
+      console.log(row);
+    }
   }
 };
 </script>
